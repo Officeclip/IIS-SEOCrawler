973ee564473c1258c8d30341ade9e2527d675c8e
Paul Stack <paulstack@hotmail.com>
2011-03-14 08:18:53 +0000

    refactoring the RunAnalysis method in crawler class - to make small methods that are more descriptive

--- SEOCrawler/Crawler.cs
+++ SEOCrawler/Crawler.cs
@@ -10,28 +10,14 @@ namespace SEOCrawler
     {
         public CrawlerReport RunAnalysis()
         {
-            var siteToScan = GetSiteToCrawl();
+            var startUrl = new Uri(GetSiteToCrawl());
 
-            // Create a URI class 
-            var startUrl = new Uri(siteToScan);
-
-            // Use the same directory as the default used by the UI 
-            var seoReportPath = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                "IIS SEO Reports");
-
-            var reportName = startUrl.Host + " " + DateTime.Now.ToString("yy-MM-dd hh-mm-ss");
-
-            // Generate a unique name 
-            var settings = new CrawlerSettings(startUrl)
-            {
-                ExternalLinkCriteria = ExternalLinkCriteria.SameFolderAndDeeper,
-                Name = reportName,
-                DirectoryCache = Path.Combine(seoReportPath, reportName)
-            };
+            var seoReportPath = BuildReportPath();
+            var reportName = BuildReportName(startUrl);
+            var crawlerSettings = BuildCrawlerSettings(startUrl, reportName, seoReportPath);
 
             // Create a new crawler and start running 
-            var crawler = new WebCrawler(settings);
+            var crawler = new WebCrawler(crawlerSettings);
             try
             {
                 crawler.Start();
@@ -55,15 +41,42 @@ namespace SEOCrawler
             catch (Exception ex)
             {
                 //logging needs added here
-                throw new ApplicationException(string.Format("Error Crawling {0}", siteToScan));
+                throw new ApplicationException(string.Format("Error Crawling {0}", startUrl));
             }
+        }
+
+        private static CrawlerSettings BuildCrawlerSettings(Uri startUrl, string reportName, string seoReportPath)
+        {
+            return new CrawlerSettings(startUrl)
+                       {
+                           ExternalLinkCriteria = ExternalLinkCriteria.SameFolderAndDeeper,
+                           Name = reportName,
+                           DirectoryCache = Path.Combine(seoReportPath, reportName)
+                       };
+        }
+
+        private static string BuildReportName(Uri startUrl)
+        {
+            return startUrl.Host + " " + DateTime.Now.ToString("yy-MM-dd hh-mm-ss");
+        }
+
+        private static string BuildReportPath()
+        {
+            //this is the default path used by IIS UI when i creates a report.
+            //saving it to this location as well means you can view the report in the UI
+
+            return Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "IIS SEO Reports");
         }
 
         private static string GetSiteToCrawl()
         {
             var siteToScan = ConfigurationManager.AppSettings.Get("siteName");
+
             if (string.IsNullOrWhiteSpace(siteToScan))
                 throw new NullReferenceException("Please specify a site URL.");
+            
             return siteToScan;
         }
     }
